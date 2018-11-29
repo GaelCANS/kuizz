855493064ead4d9f886743b00bf8eca17646e32d
Gael <gael.levant@ca-normandie-seine.fr>
2018-11-29 14:17:07 +0100

    collapse question/answer

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -2,7 +2,7 @@
     <div class="row">
         <div class="col-md-4">
             <div class="form-group">
-                <h6>Question n°<span class="order">{{$question->order}}</span></h6>
+                <h6 data-toggle="collapse" data-target="#content-{{$question->id}}">Question n°<span class="order">{{$question->order}}</span></h6>
                 {!! Form::text( 'question[update]['.$question->id.'][wording]' , $question->wording , array( 'class' => 'form-control' , 'placeholder' => "Votre question" ) ) !!}
                 {!! Form::hidden('question[update]['.$question->id.'][order]' , $question->order , array('class' => 'question-order')) !!}
 
@@ -12,32 +12,35 @@
         </div>
     </div>
 
-    <div class="row">
-        <div class="col-md-4">
-            <div class="form-group">
+    <div class="collapse"  id="content-{{$question->id}}">
 
-                {!! Form::text( 'question[update]['.$question->id.'][comment]' , $question->comment , array( 'class' => 'form-control' , 'placeholder' => "Un commentaire ?" ) ) !!}
+        <div class="row">
+            <div class="col-md-4">
+                <div class="form-group">
 
-                <button type="button" class="btn btn-outline-secondary icon-btn del-question" data-question="{{$question->id}}"  data-link="{{action('QuestionController@destroy' , array('id' => $question->id))}}"><i class="mdi mdi-delete"></i></button>
+                    {!! Form::text( 'question[update]['.$question->id.'][comment]' , $question->comment , array( 'class' => 'form-control' , 'placeholder' => "Un commentaire ?" ) ) !!}
 
-                <div class="row">
-                    <div class="form-group">
-                        <div class="col-md-12">
-                            <button type="button" class="btn btn-secondary add-answer" data-link="{{ action('AnswerController@store') }}">
-                                <i class="fa fa-fw fa-save"></i>Nouvelle réponse
-                            </button>
+                    <button type="button" class="btn btn-outline-secondary icon-btn del-question" data-question="{{$question->id}}"  data-link="{{action('QuestionController@destroy' , array('id' => $question->id))}}"><i class="mdi mdi-delete"></i></button>
+
+                    <div class="row">
+                        <div class="form-group">
+                            <div class="col-md-12">
+                                <button type="button" class="btn btn-secondary add-answer" data-link="{{ action('AnswerController@store') }}">
+                                    <i class="fa fa-fw fa-save"></i>Nouvelle réponse
+                                </button>
+                            </div>
                         </div>
                     </div>
-                </div>
 
+                </div>
             </div>
         </div>
-    </div>
 
-    <ul class="container-answers sortable">
-        @forelse($question->answers as $answer)
-            @include('answers.show')
-            @empty
-        @endforelse
-    </ul>
+        <ul class="container-answers sortable">
+            @forelse($question->answers as $answer)
+                @include('answers.show')
+                @empty
+            @endforelse
+        </ul>
+    </div>
 </li>
